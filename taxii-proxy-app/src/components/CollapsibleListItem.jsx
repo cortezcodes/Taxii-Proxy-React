897820df1fd6178e5cf10222ca627692852cfda1
Cortez McCrary <cmccrary32@gatech.edu>
2024-02-19 22:35:54 -0500

    Updated List Item Json styling

--- taxii-proxy-app/src/components/CollapsibleListItem.jsx
+++ taxii-proxy-app/src/components/CollapsibleListItem.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import JsonFormatter from 'react-json-formatter';
 
 function CollapsibleListItem(props){
+
+    const jsonStyle = {
+        propertyStyle: { color: 'red' },
+        stringStyle: { color: 'green' },
+        numberStyle: { color: 'darkorange' }
+    }
+
     console.log(props.stixObject);
     return (<li className="list-item-no-bullet">
-        <pre>{JSON.stringify(props.stixObject, null, 2)}</pre>
+        <JsonFormatter json={props.stixObject} tabWith={4} jsonStyle={jsonStyle}/>
     </li>);
 }
 
