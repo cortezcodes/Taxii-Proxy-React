4b952ffb8c05fe43e430fb8f3eb0786f35a4a0fa
Cortez McCrary <47889867+cortezcodes@users.noreply.github.com>
2024-03-10 20:59:04 -0400

    stuck on visualizer

--- taxii-proxy-app/src/components/NetworkDiagram.jsx
+++ taxii-proxy-app/src/components/NetworkDiagram.jsx
@@ -3,7 +3,10 @@ import StixParser from "../utils/StixParser";
 import * as d3 from "d3";
 
 /**
- * Force Simulator Component for display STIX objects in a network graph. 
+ * Force Simulator Component for display STIX objects in a network graph. This code was
+ * derived from d3.js documentation and examples.
+ * Reference: Bostock, M. (2023, November 10). Zoomable Scatterplot. Observable. https://observablehq.com/@d3/zoomable-scatterplot  
+ * Reference: Laranjeira, B. (2021, January 16). D3 v6 force-directed graph with Directional Straight Arrows. Observable. https://observablehq.com/@brunolaranjeira/d3-v6-force-directed-graph-with-directional-straight-arrow 
  * @param {number} height - height dimension of network graph, defaults to 600
  * @param {number} width - width dimension of network graph, defaults to 800
  * @param {json} stixbundle - stix bundle to be processed into a network graph
@@ -20,20 +23,29 @@ function NetworkDiagram({height=600 , width=800, stixBundle}){
         //d3.js will mutate the links and nodes so it is good practice to make copies
         const links = data.links.map((l) => ({...l}));
         const nodes = data.nodes.map((n) => ({...n}));
-
-        console.log(links);
-        console.log(nodes);
         
         const svg = d3.select(svgRef.current);
-        const color = d3.scaleOrdinal(d3.schemeCategory10);
+        const color = d3.scaleOrdinal(d3.schemeCategory10); //TODO Change this to specific images
 
-        d3.select(svgRef.current).selectAll("*").remove();
+        // const zoom = d3.zoom()
+        //     .scaleExtent([1, 10])
+        //     .on("zoom", (e) => {
+        //         node.attr('transform', e.transform).attr("stroke-width", 5 / e.transform.k);
+                
+        //     });
+
+        // svg.call(zoom);
+
+        // d3.select(svgRef.current).selectAll("*").remove();
+        svg.selectAll("*").remove();
 
 
         // Setup Simulaton
         const simulation = d3.forceSimulation(nodes)
             .force('link', d3.forceLink(links).id((d) => d.id))
-            .force('charge', d3.forceManyBody())
+            .force('charge', d3.forceManyBody().strength(-400))
+            .force("x", d3.forceX()) 
+            .force("y", d3.forceY())
             .force('center', d3.forceCenter(width/2,height/2))
             .on("tick", () => {
                 link.attr('x1', d => d.source.x)
@@ -48,26 +60,28 @@ function NetworkDiagram({height=600 , width=800, stixBundle}){
         //Arrows for links
         svg.append("defs").selectAll("marker")
             .data(["arrow"])
-            .enter().append("marker")
-            .attr("id", function(d) { return d; })
+            .enter()
+            .append("marker")
+            .attr("id", d => {return d;})
             .attr("viewBox", "0 -5 10 10")
             .attr("refX", 20)
             .attr("refY", 0)
-            .attr("markerWidth", 6)
-            .attr("markerHeight", 6)
-            .attr('orient', 'auto')
+            .attr("markerWidth", 8)
+            .attr("markerHeight", 8)
+            .attr("orient", "auto")
             .append("path")
-            .attr("d", "M 0 ,-5 L 10 ,0 L 0, 5")
+            .attr("fill", d => color(d.group))
+            .attr("d", 'M0,-5L10,0L0,5');
 
         // Add a line for each link, and a circle for each node
         const link = svg.append('g')
-            .attr("stroke", "#999")
-            .attr("stroke-opacity", 0.6)
             .selectAll()
             .data(links)
             .join("line")
-            .attr("stroke-width", d => Math.sqrt(d.value))
+            .attr("stroke", d => color(d.type))
+            .attr("stroke-width", 1.5)
             .attr('marker-end', 'url(#arrow)');
+
 
         const node = svg.append('g')
             .attr("stroke", "#fff")
@@ -94,5 +108,7 @@ function NetworkDiagram({height=600 , width=800, stixBundle}){
         </div>
     )
 }
+
+
 
 export default NetworkDiagram;
